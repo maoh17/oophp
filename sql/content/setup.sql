2105f26a77fe5be782e27ec4c57fc3b56891489d
Magnus Ohlin <maoh17@student.bth.se>
2018-05-29 22:49:02 +0200

    Changed table name

--- sql/content/setup.sql
+++ sql/content/setup.sql
@@ -17,8 +17,8 @@ SET NAMES utf8;
 --
 -- Create table for Content
 --
-DROP TABLE IF EXISTS `content`;
-CREATE TABLE `content`
+DROP TABLE IF EXISTS `content06`;
+CREATE TABLE `content06`
 (
   `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   `path` CHAR(120) UNIQUE,
@@ -42,7 +42,7 @@ CREATE TABLE `content`
   `deleted` DATETIME DEFAULT NULL
 ) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
 
-INSERT INTO `content` (`path`, `slug`, `type`, `title`, `data`, `filter`) VALUES
+INSERT INTO `content06` (`path`, `slug`, `type`, `title`, `data`, `filter`) VALUES
     ("hem", null, "page", "Hem", "Detta är min hemsida. Den är skriven i [url=http://en.wikipedia.org/wiki/BBCode]bbcode[/url] vilket innebär att man kan formattera texten till [b]bold[/b] och [i]kursiv stil[/i] samt hantera länkar.\n\nDessutom finns ett filter 'nl2br' som lägger in <br>-element istället för \\n, det är smidigt, man kan skriva texten precis som man tänker sig att den skall visas, med radbrytningar.", "bbcode,nl2br"),
     ("om", null, "page", "Om", "Detta är en sida om mig och min webbplats. Den är skriven i [Markdown](http://en.wikipedia.org/wiki/Markdown). Markdown innebär att du får bra kontroll över innehållet i din sida, du kan formattera och sätta rubriker, men du behöver inte bry dig om HTML.\n\nRubrik nivå 2\n-------------\n\nDu skriver enkla styrtecken för att formattera texten som **fetstil** och *kursiv*. Det finns ett speciellt sätt att länka, skapa tabeller och så vidare.\n\n###Rubrik nivå 3\n\nNär man skriver i markdown så blir det läsbart även som textfil och det är lite av tanken med markdown.", "markdown"),
     ("blogpost-1", "valkommen-till-min-blogg", "post", "Välkommen till min blogg!", "Detta är en bloggpost.\n\nNär det finns länkar till andra webbplatser så kommer de länkarna att bli klickbara.\n\nhttp://dbwebb.se är ett exempel på en länk som blir klickbar.", "link,nl2br"),
@@ -61,7 +61,7 @@ Avsluter med en lista som formatteras till ul/li med markdown.
         * Item 1
         * Item 2", "bbcode,link,markdown");
 
-SELECT `id`, `path`, `slug`, `type`, `title`, `created` FROM `content`;
+SELECT `id`, `path`, `slug`, `type`, `title`, `created` FROM `content06`;
 
 
 --
