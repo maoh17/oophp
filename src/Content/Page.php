7d21c720d24472da07b053b583b2a87153753a96
Magnus Ohlin <maoh17@student.bth.se>
2018-05-29 22:49:45 +0200

    Changed table name

--- src/Content/Page.php
+++ src/Content/Page.php
@@ -20,14 +20,14 @@ class Page extends Content
 
     public function createPage($title)
     {
-        $sql = "INSERT INTO content (title, type) VALUES (?, ?);";
+        $sql = "INSERT INTO content06 (title, type) VALUES (?, ?);";
         $this->db->execute($sql, [$title, "page"]);
     }
 
 
     public function updatePage($title, $path, $data, $type, $filter, $publish, $id)
     {
-        $sql = "UPDATE content SET title=?, path=?, data=?, type=?, filter=?, published=? WHERE id = ?;";
+        $sql = "UPDATE content06 SET title=?, path=?, data=?, type=?, filter=?, published=? WHERE id = ?;";
         $this->db->execute($sql, [$title, $path, $data, $type, $filter, $publish, $id]);
     }
 
@@ -39,7 +39,7 @@ SELECT
 *,
 DATE_FORMAT(COALESCE(updated, published), '%Y-%m-%dT%TZ') AS published_iso8601,
 DATE_FORMAT(COALESCE(updated, published), '%Y-%m-%d') AS published
-FROM content
+FROM content06
 WHERE type=?
 AND path=?
 ;
